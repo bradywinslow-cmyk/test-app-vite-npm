70b01c5c964cd1fd02e0d595e95a20df236e9c68
Brady Winslow <brady.winslow@sprig.com>
2026-05-27 14:08:23 -0700

    update configuration of Sprig

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import { sprig } from '@sprig-technologies/sprig-browser';
 
 export const Sprig = sprig.configure({
-  environmentId: window.location.host === 'localhost:5173' ? 'Fo7CM2CT4uiD' : 'iUPkMKvu11OT'
+  environmentId: 'iUPkMKvu11OT'
 })
 
 createRoot(document.getElementById('root')).render(
