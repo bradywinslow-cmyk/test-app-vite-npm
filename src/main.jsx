aa35627604f63892d8f6a7d8031dbc1ec250e4ff
Brady Winslow <brady.winslow@sprig.com>
2026-05-27 16:36:20 -0700

    change to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import { sprig } from '@sprig-technologies/sprig-browser';
 
 export const Sprig = sprig.configure({
-  environmentId: 'iUPkMKvu11OT',
+  environmentId: 'Fo7CM2CT4uiD'
 })
 
 createRoot(document.getElementById('root')).render(
